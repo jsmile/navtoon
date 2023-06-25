fb015416e402b27cb22a65ee796b4fbf55b51ad7
jsmile <jsmile@naver.com>
2023-06-25 18:18:24 +0900

    stateful screen with parameter

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:navtoon/models/webtoon_detail_model.dart';
+import 'package:navtoon/models/webtoon_episode_model.dart';
+import 'package:navtoon/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String id, title, thumb;
 
   const DetailScreen({
@@ -11,13 +14,29 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoonDetail;
+  late Future<List<WebtoonEpisodeModel>> webtoonEpisodes;
+
+  @override
+  void initState() {
+    super.initState();
+
+    webtoonDetail = ApiService.getWebtoonDetailById(widget.id);
+    webtoonEpisodes = ApiService.getWebtoonEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.w600,
@@ -33,7 +52,7 @@ class DetailScreen extends StatelessWidget {
               height: 30,
             ),
             Hero(
-              tag: id,
+              tag: widget.id,
               child: Container(
                 width: 180,
                 clipBehavior: Clip.hardEdge,
@@ -47,7 +66,7 @@ class DetailScreen extends StatelessWidget {
                       ),
                     ]),
                 child: Image.network(
-                  thumb,
+                  widget.thumb,
                   headers: const {
                     'Referer': 'https://comic.naver.com', // 403 오류 방지
                   },
@@ -57,7 +76,7 @@ class DetailScreen extends StatelessWidget {
             const SizedBox(
               height: 10,
             ),
-            Text(title),
+            Text(widget.title),
           ],
         ),
       ),
